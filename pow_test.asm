263ab2c0165ffe18c843eb2fad4a2d9fb007618a
Fellipe-Tripovichy <fellipe.tripovichy@gmail.com>
2023-12-20 21:03:14 -0300

    Arquivos

---
+++ pow_test.asm
@@ -0,0 +1,21 @@
+jmp main
+
+;Codigo principal
+main:
+
+	loadn r0, #20
+	loadn r1, #2 ;400
+	pow r0, r0, r1
+
+
+	loadn r0, #2
+	loadn r1, #5 ; 32
+	pow r0, r0, r1
+
+
+	loadn r0, #10
+	loadn r1, #4 ; 10000
+	pow r0, r0, r1
+
+
+	halt
